ee779135f2cd0c318dabb37a78162774b3ad1b3b
akrajput562 <66029756+akrajput562@users.noreply.github.com>
2023-11-09 07:49:09 +0000

    conversion till now

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,8 +1,28 @@
-let score = 33
+let score = undefined
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 
 let valueInnumber = Number (score)
-console.log(typeof score);
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber);
+
+
+// "33"=>33
+//"33abc"=>nan
+//true=>1 ; false =>0
+
+let isLoggedIn= "akgsd"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1=>true; 0=>false
+// ""=>false
+// "aksds"=>true
+
+let someNumber = 33 
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
