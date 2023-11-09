f18563555768f59c4a8ce5eeb92e11ac96c1b47c
akrajput562 <66029756+akrajput562@users.noreply.github.com>
2023-11-09 07:31:50 +0000

    Basic Definaion

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 19942;
+let accountEmail="kartonn@hd.com"
+var accoundPassword="02kfjri02"
+accountCity="Jaipur"// nhi krna chayie but kr skte
+let accountState;//if not assigned then undefined
+/*
+Prefer not to use var
+because of issue of block,
+// accountId=2 // Not Allowed to change
+*/
+accountEmail="Hd@Gmai.com"
+accoundPassword="vmskfjfk"
+accountCity="babgalore"
+console.log(accountId);
+console.table([accountId,accoundPassword,accountEmail,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";//treat all Js Code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+let name="gargo"
+let age = 18
+let isLoggedIn= false;
+let state;
+
+//number =>2 to power 53
+//bigint
+//string=>""
+//boolean= ha ya na// true|false
+//null=> standalone value
+//undefined=>
+//symbol:for uniqness
+
+//object 
+console.log(typeof undefined);
+//null is object but undefined is undefiended
